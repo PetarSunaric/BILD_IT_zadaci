8c83c948b707fe0f89868f9ee05aaffec6a162f2
Tijana18 <tijanafilipovic@hotmail.com>
2016-08-02 22:53:20 +0200

    Zadatak_5

--- src/zadaci_02_08_2016/Zadatak_5.java
+++ src/zadaci_02_08_2016/Zadatak_5.java
@@ -3,6 +3,7 @@ package zadaci_02_08_2016;
 public class Zadatak_5 {
 
 	public static void coin(int n) {
+		
 		int heads = 0, tails = 0;
 		/* Bacamo novcic random brojem 0 i 1, ispitamo na koji je broj palo, i
 		 brojimo, tako sto odredimo broj 0 zapismo, i 1 za glavu*/
