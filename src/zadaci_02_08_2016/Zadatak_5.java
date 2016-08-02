50aaa1ce584b271877e685fdede17a2a8fb9cdea
Tijana <Tijana@Tijana-PC>
2016-08-02 22:50:09 +0200

    zadaci_02_08_2016

--- src/zadaci_02_08_2016/Zadatak_5.java
+++ src/zadaci_02_08_2016/Zadatak_5.java
@@ -4,8 +4,10 @@ public class Zadatak_5 {
 
 	public static void coin(int n) {
 		int heads = 0, tails = 0;
-		/* Bacamo novcic random brojem 0 i 1, ispitamo na koji je broj palo, i
-		 brojimo, tako sto odredimo broj 0 zapismo, i 1 za glavu*/
+		/*
+		 * Bacamo novcic random brojem 0 i 1, ispitamo na koji je broj palo, i
+		 * brojimo, tako sto odredimo broj 0 zapismo, i 1 za glavu
+		 */
 		for (int i = 0; i < n; i++) {
 			int coin = (int) (Math.random() * 2);
 			if (coin == 0) {
@@ -14,18 +16,19 @@ public class Zadatak_5 {
 				heads++;
 			}
 		}
-		//Ispisujemo korisniku koliko je puta novcic pao na koju stranu
+		// Ispisujemo korisniku koliko je puta novcic pao na koju stranu
 		System.out.println("Novcic je pao " + tails + " na stranu pismo, a " + heads + " na stranu glava!");
 	}
 
 	public static void main(String[] args) {
 
 		java.util.Scanner input = new java.util.Scanner(System.in);
-		//Pitamo korisnika koliko puta zeli da se novcic baci i uzimamo unos
+		
+		// Pitamo korisnika koliko puta zeli da se novcic baci i uzimamo unos
 		System.out.println("Koliko puta zelite da bacite novcic? ");
 		int n = input.nextInt();
-		
-		//Pozivamo metodu
+
+		// Pozivamo metodu
 		coin(n);
 	}
 }
