068a36e6be5adae73723353e9d380a31762ea693
Tijana <Tijana@Tijana-PC>
2016-08-05 11:17:45 +0200

    zadaci_04_08_2016

--- src/zadaci_04_08_2016/Zadatak_3.java
+++ src/zadaci_04_08_2016/Zadatak_3.java
@@ -1,31 +1,23 @@
 package zadaci_04_08_2016;
 
-public class Zadatak_3 {
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
-	public static void displaySortedNumbers(double num1, double num2, double num3) {
-		// Sortiramo brojeve od najmanjeg ka najvecem 
-		if (num1 < num2 && num1 < num3 && num2 < num3) {
-			System.out.println(num1 + " " + num2 + " " + num3);
-		} else if (num1 <= num2 && num1 <= num3 && num3 <= num2) {
-			System.out.println(num1 + " " + num3 + " " + num2);
-		} else if (num2 <= num1 && num2 <= num3 && num1 <= num3) {
-			System.out.println(num2 + " " + num1 + " " + num3);
-		} else if (num2 <= num1 && num2 <= num3 && num3 <= num1) {
-			System.out.println(num2 + " " + num3 + " " + num1);
-		} else if (num3 <= num1 && num3 <= num2 && num2 <= num1) {
-			System.out.println(num3 + " " + num2 + " " + num1);
-		} else if (num3 <= num1 && num3 <= num2 && num1 <= num2) {
-			System.out.println(num3 + " " + num1 + " " + num2);
-		}
-	}
+public class Zadatak_3 {
 
 	public static void main(String[] args) {
 		java.util.Scanner input = new java.util.Scanner(System.in);
+		int[] array = new int[3];
+		
 		// Unos i poziv metode
 		System.out.println("Molimo unesite tri broja: ");
-		int num1 = input.nextInt();
-		int num2 = input.nextInt();
-		int num3 = input.nextInt();
-		displaySortedNumbers(num1, num2, num3);
+		for (int i = 0; i < 3; i++) {
+			array[i] = input.nextInt();
+		}
+		//Pozivamo metodu i stampamo niz
+		Arrays.sort(array);
+		for (int i = 0; i < 3; i++) {
+			System.out.print(array[i] + " ");
+		}
 	}
 }
