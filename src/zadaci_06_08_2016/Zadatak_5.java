c51e4c182f185b64c40dab7f52a389ee202613ea
Tijana <Tijana@Tijana-PC>
2016-08-07 14:23:44 +0200

    zadaci_06_08_2016

--- src/zadaci_06_08_2016/Zadatak_5.java
+++ src/zadaci_06_08_2016/Zadatak_5.java
@@ -5,15 +5,18 @@ import java.text.DecimalFormat;
 public class Zadatak_5 {
 
 	public static void main(String[] args) {
+		// Uzimamo potrebne podatke od korisnika
 		java.util.Scanner input = new java.util.Scanner(System.in);
 		System.out.println("Molimo unesite ubrzanje aviona u m/s: ");
-		double v = input.nextDouble();
-		System.out.println("Molimo unesite brzinu pri kojoj avion uzlijece u m/s: ");
 		double a = input.nextDouble();
-		
+		System.out.println("Molimo unesite brzinu pri kojoj avion uzlijece u m/s: ");
+		double v = input.nextDouble();
+
+		// Racunamo minimalnu duzinu piste pomocu formule, prebacujemo je u
+		// odredjen format, te ispisujemo
 		double minimalnaDuzinaPiste = ((v * v) / (2 * a));
 		minimalnaDuzinaPiste = Double.parseDouble(new DecimalFormat("##.###").format(minimalnaDuzinaPiste));
 		System.out.println("Minimalna duzina piste iznosi " + minimalnaDuzinaPiste);
-		
+
 	}
 }
