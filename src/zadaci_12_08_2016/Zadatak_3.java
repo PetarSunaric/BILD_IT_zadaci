bf4332a2ac3f9b6293bba8487adfe71d54ca0e37
Tijana <Tijana@Tijana-PC>
2016-08-13 18:49:07 +0200

    zadaci_12_08_2016

--- src/zadaci_12_08_2016/Zadatak_3.java
+++ src/zadaci_12_08_2016/Zadatak_3.java
@@ -4,9 +4,7 @@ public class Zadatak_3 {
 
 	public static int isPrime(int n) {
 		// Provjeravamo da li je broj prost
-		if (n % 2 == 0)
-			return 0;
-		for (int i = 3; i * i <= n; i += 2) {
+		for (int i = 2; i < n; i++) {
 			if (n % i == 0)
 				return 0;
 		}
