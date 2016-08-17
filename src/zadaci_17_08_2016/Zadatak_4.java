8a048102ff7b6e77c29708e4ec031e044bba6db7
Tijana <Tijana@Tijana-PC>
2016-08-17 23:47:31 +0200

    zadaci_17_08_2016

--- src/zadaci_17_08_2016/Zadatak_4.java
+++ src/zadaci_17_08_2016/Zadatak_4.java
@@ -3,12 +3,13 @@ package zadaci_17_08_2016;
 public class Zadatak_4 {
 
 	public static String format(int number, int width) {
+		// Dodajemo nule na broj
 		return String.format("%0" + width + "d", number);
 	}
 
 	public static void main(String[] args) {
 		java.util.Scanner input = new java.util.Scanner(System.in);
-
+		// Uzimamo unos i pozivamo metodu
 		System.out.println("Molimo unesite vas broj: ");
 		int number = input.nextInt();
 		System.out.println("Molimo unesite duzinu broja: ");
--- src/zadaci_17_08_2016/Zadatak_5.java
+++ src/zadaci_17_08_2016/Zadatak_5.java
@@ -6,10 +6,13 @@ public class Zadatak_5 {
 
 	public static void main(String[] args) {
 		java.util.Scanner input = new java.util.Scanner(System.in);
+		// Pravimo listu u koju se ne unose dupli brojevi,i od korisnika uzimamo
+		// brojeve
 		HashSet<Integer> list = new HashSet<Integer>();
-		for(int i = 0; i < 10; i++){
-			list.add(input.nextInt());		
+		for (int i = 0; i < 10; i++) {
+			list.add(input.nextInt());
 		}
+		// Stampamo listu i velicinu liste
 		System.out.println("Jedinstveni brojevi su " + list);
 		System.out.println("Jedinstvenih brojeva imamo " + list.size());
 	}
